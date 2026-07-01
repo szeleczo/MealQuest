8be60e3f863635bb2200f28665d6a69de98a025b
szeleczo <szeleczo@gmail.com>
2026-07-01 19:10:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-/* MealQuest service worker v0.4.1 */
-const CACHE = 'mealquest-v0-4-1';
-const ASSETS = ['./?source=pwa','./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./assets/scene-fat.png','./assets/scene-medium.png','./assets/scene-lean.png'];
+/* MealQuest service worker v0.4.2 */
+const CACHE = 'mealquest-v0-4-2';
+const ASSETS = ['./?source=pwa','./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./assets/scene-fat.png','./assets/scene-medium.png','./assets/scene-lean.png','./assets/scene-female-fat.png','./assets/scene-female-medium.png','./assets/scene-female-lean.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
